ae3992b022f6be7397899a6398aece59971f1463
Sambathrorth <kongsambathrorth@gmail.om>
2025-03-01 16:30:50 +0700

    Update Excercise 2

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -4,7 +4,48 @@ class Program
 {
     static void Main(string[] args)
     {
-        // This pre 2
-        Console.WriteLine("Hello World! Pre1 World.");
+        Console.Write("Enter your grade percentage: ");
+        string valueFromUser = Console.ReadLine();
+        int GradePercent  = int.Parse(valueFromUser);
+
+        string letter = "";
+
+        if (GradePercent >= 90)
+        {
+           letter = "A";
+          
+        }
+        else if (GradePercent>=80)
+        {
+            letter = "B";
+        }
+
+        else if (GradePercent>=70)
+        {
+            letter = "C";
+        }
+
+        else if (GradePercent>=60)
+        {
+            letter = "D";
+        }
+
+        else
+        {
+            letter = "F";
+        }
+
+        Console.WriteLine($"Your grade is: {letter}");
+
+        if (GradePercent>= 70)
+        {
+            Console.WriteLine("Congratulation! You passed");
+        }
+
+        else
+        {
+            Console.WriteLine("Wish you good luck for next time");
+        }
+
     }
 }
