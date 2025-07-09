83c5afd7550633b54aa5f34043cfcf46d72ac31d
kongsambathrorth <you@example.com>
2025-07-10 01:38:12 +0700

    Update

--- week01/Exercise1/Program.cs
+++ week01/Exercise1/Program.cs
@@ -5,7 +5,7 @@ class Program
     static void Main(string[] args)
     {
         // Ask the user for their name.
-        Console.Write("What is your firt name? ");
+        Console.Write("Hello! What is your firt name? ");
         string first = Console.ReadLine();
 
         Console.Write("What is your last name? ");
