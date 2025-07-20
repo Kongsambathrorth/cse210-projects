a6eba7e209cfc772af9b9101db72f560d0af8afc
kongsambathrorth <you@example.com>
2025-07-20 18:01:04 +0700

    update

--- week02/Resumes/Program.cs
+++ week02/Resumes/Program.cs
@@ -7,17 +7,17 @@ class Program
         Job job1 = new Job();
         job1._jobTitle = "Software Engineer";
         job1._company = "Cam Developer";
-        job1._startYear = 2019;
+        job1._startYear = 2020;
         job1._endYear = 2022;
 
         Job job2 = new Job();
         job2._jobTitle = "Coordinator";
         job2._company = "Sony";
         job2._startYear = 2022;
-        job2._endYear = 2023;
+        job2._endYear = 2025;
 
         Resume myResume = new Resume();
-        myResume._name = "Allison Rose";
+        myResume._name = "Sambath Rorth";
 
         myResume._jobs.Add(job1);
         myResume._jobs.Add(job2);
